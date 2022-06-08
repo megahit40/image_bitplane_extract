a321edf52c9d10e3924dc2f2502ffe9b4d4006a0
megahit40 <75255544+megahit40@users.noreply.github.com>
2022-06-08 21:40:27 +0000

    comment

--- bitplane_extract.py
+++ bitplane_extract.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 	for channel in range(3):
 		for bit_pos in range(8):
 			out_pixels = get_planes(img.getchannel(channel).getdata(),bit_pos)
-			## Black and white ("1"):
+			## Black and white single channel ("1"):
 			out_img = Image.new("1", img.size)
 			out_img.putdata(out_pixels)
 			out_img.save(f"{'RGB'[channel]}-{bit_pos}.png")
